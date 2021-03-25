5b32f4fd0ca0a96a2dea0c4a6033a15039c9922f
Chrisadd23 <74181009+Chrisadd23@users.noreply.github.com>
2021-03-25 19:50:22 +0100

    Raumschiff und Hintergrund

--- WpfAppReturnOfSASHE/MainWindow.xaml.cs
+++ WpfAppReturnOfSASHE/MainWindow.xaml.cs
@@ -22,7 +22,17 @@ namespace WpfAppReturnOfSASHE
     /// </summary>
     public partial class MainWindow : Window
     {
-        int backgroundspeed = 12;
+        int backgroundspeed = 7;
+        int backgroundspeedGalaxy = 10;
+
+
+        ImageBrush hg1 = new ImageBrush();
+        ImageBrush zwischenraum1 = new ImageBrush();
+        ImageBrush hg2 = new ImageBrush();
+        ImageBrush zwischenraum2 = new ImageBrush();
+        ImageBrush hgGalaxy1 = new ImageBrush();
+        ImageBrush hgGalaxy2 = new ImageBrush();
+        ImageBrush hgDecke = new ImageBrush();
 
         DispatcherTimer gameTimer = new DispatcherTimer();
         public MainWindow()
@@ -44,47 +54,90 @@ namespace WpfAppReturnOfSASHE
              * einer bestimmten Reihenfolge and das Ende gehängt.
              * 
              */
+     
 
             Canvas.SetLeft(hintergrund1, Canvas.GetLeft(hintergrund1) - backgroundspeed);
-            Canvas.SetLeft(hintergrundZwischenraum, Canvas.GetLeft(hintergrundZwischenraum) - backgroundspeed);
-            Canvas.SetLeft(hintergrund3, Canvas.GetLeft(hintergrund3) - backgroundspeed);
+            Canvas.SetLeft(hintergrundZwischenraum1, Canvas.GetLeft(hintergrundZwischenraum1) - backgroundspeed);
+            Canvas.SetLeft(hintergrund2, Canvas.GetLeft(hintergrund2) - backgroundspeed);
+            Canvas.SetLeft(hintergrundZwischenraum2, Canvas.GetLeft(hintergrundZwischenraum2) - backgroundspeed);
+            Canvas.SetLeft(hintergrundDach1, Canvas.GetLeft(hintergrundDach1) - backgroundspeed);
+            Canvas.SetLeft(hintergrundDach2, Canvas.GetLeft(hintergrundDach2) - backgroundspeed);
+            Canvas.SetLeft(hintergrundBoden1, Canvas.GetLeft(hintergrundBoden1) - backgroundspeed);
+            Canvas.SetLeft(hintergrundBoden2, Canvas.GetLeft(hintergrundBoden2) - backgroundspeed);
+
+            Canvas.SetLeft(hintergrundGalaxy1, Canvas.GetLeft(hintergrundGalaxy1) - backgroundspeedGalaxy);
+            Canvas.SetLeft(hintergrundGalaxy2, Canvas.GetLeft(hintergrundGalaxy2) - backgroundspeedGalaxy);
 
 
-            if (Canvas.GetLeft(hintergrund1) < -289 - (1579 - 289))
+            if (Canvas.GetLeft(hintergrund1) < -1579 )
             {
-                Canvas.SetLeft(hintergrund1, 1770 
-            );
+                Canvas.SetLeft(hintergrund1, Canvas.GetLeft(hintergrundZwischenraum2) + hintergrundZwischenraum2.Width -80);
             }
-            /*
-            if (Canvas.GetLeft(hintergrundZwischenraum) < -289 - (376))
+            if(Canvas.GetLeft(hintergrundZwischenraum1) < -480)
             {
-                Canvas.SetLeft(hintergrundZwischenraum, 1770);
+                Canvas.SetLeft(hintergrundZwischenraum1, Canvas.GetLeft(hintergrund1) + hintergrund1.Width - 80);
             }
-            if (Canvas.GetLeft(hintergrund3) < -289 - (1579 - 289))
+            if(Canvas.GetLeft(hintergrund2) < -1579)
             {
-                Canvas.SetLeft(hintergrund3, 1770);
+                Canvas.SetLeft(hintergrund2, Canvas.GetLeft(hintergrundZwischenraum1) + hintergrundZwischenraum1.Width -80);
             }
-            */
+            if(Canvas.GetLeft(hintergrundZwischenraum2) < -480)
+            {
+                Canvas.SetLeft(hintergrundZwischenraum2, Canvas.GetLeft(hintergrund2) + hintergrund2.Width -80 );
+            }
+            if(Canvas.GetLeft(hintergrundGalaxy1) < -5100)
+            {
+                Canvas.SetLeft(hintergrundGalaxy1, Canvas.GetLeft(hintergrundGalaxy2) + 5000);
+            }
+            if(Canvas.GetLeft(hintergrundGalaxy2) < -5100)
+            {
+                Canvas.SetLeft(hintergrundGalaxy2, Canvas.GetLeft(hintergrundGalaxy1) + 5000);
+            }
+            if(Canvas.GetLeft(hintergrundDach1) < -5100)
+            {
+                Canvas.SetLeft(hintergrundDach1, Canvas.GetLeft(hintergrundDach2) + 5000);
+            }
+            if(Canvas.GetLeft(hintergrundDach2) < -5100)
+            {
+                Canvas.SetLeft(hintergrundDach2, Canvas.GetLeft(hintergrundDach1) + 5000);
+            }
+            if (Canvas.GetLeft(hintergrundBoden1) < -5100)
+            {
+                Canvas.SetLeft(hintergrundBoden1, Canvas.GetLeft(hintergrundBoden2) + 5000);
+            }
+            if (Canvas.GetLeft(hintergrundBoden2) < -5100)
+            {
+                Canvas.SetLeft(hintergrundBoden2, Canvas.GetLeft(hintergrundBoden1) + 5000);
+            }
+
         }
 
         public void HintergrundEinrichten()
         {
-            hintergrund1.Fill = new ImageBrush
-            {
-                ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Raum1.png"))
-            };
+            hg1.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Raum1.png"));
+            zwischenraum1.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Zwischenraum.png"));
+            hg2.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Raum3.png"));
+            zwischenraum2.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Zwischenraum.png"));
+            hgGalaxy1.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Hintergrund.png"));
+            hgGalaxy2.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Hintergrund.png"));
+            hgDecke.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Decke.png"));
 
-            hintergrundZwischenraum.Fill = new ImageBrush
-            {
-                ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Zwischenraum.png"))
+            hintergrund1.Fill = hg1;
+            hintergrundZwischenraum1.Fill = zwischenraum1;
+            hintergrund2.Fill = hg2;
+            hintergrundZwischenraum2.Fill = zwischenraum2;
+            hintergrundGalaxy1.Fill = hgGalaxy1;
+            hintergrundGalaxy2.Fill = hgGalaxy2;
+            hintergrundDach1.Fill = hgDecke;
+            hintergrundDach2.Fill = hgDecke;
+            hintergrundBoden1.Fill = hgDecke;
+            hintergrundBoden2.Fill = hgDecke;
 
-            };
+
+
             
 
-            hintergrund3.Fill = new ImageBrush
-            {
-                ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/Bilder/HintergrundBilder/Raum2.png"))
-            };
+
         }
 
       
